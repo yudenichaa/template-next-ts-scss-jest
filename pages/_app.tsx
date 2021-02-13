d40b9e4b7c7cfcbec2d62d273795827641d371c1
yudenichaa <yudenichalexandr@yandex.ru>
2021-02-13 16:40:34 +0300

    update template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.scss';
+import Head from 'next/head';
+import '../styles/normalize.css';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <html lang="en" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Template</title>
-        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
       </Head>
     </>
   );
